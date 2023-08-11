05b077b1b57972539e98ead8f4607c441e62c80a
Krisztina Pelei <krisztina.pelei@nttdata.com>
2023-08-11 16:47:30 +0200

    add logging

--- src/test/java/com/example/github_actions_test/GithubActionsTestApplicationTests.java
+++ src/test/java/com/example/github_actions_test/GithubActionsTestApplicationTests.java
@@ -1,15 +1,18 @@
 package com.example.github_actions_test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Slf4j
 class GithubActionsTestApplicationTests {
 
 	@Test
 	void successfulTest() {
+		log.info("Successful test");
 		System.out.println("Successful test");
 		assertEquals(true, true);
 	}
