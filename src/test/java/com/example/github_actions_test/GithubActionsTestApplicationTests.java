019a76b33d0aaed7f9c845af810acb6a0e2cdaa4
Krisztina Pelei <krisztina.pelei@nttdata.com>
2023-08-11 16:33:43 +0200

    add Run tests steps, expected to fail

--- src/test/java/com/example/github_actions_test/GithubActionsTestApplicationTests.java
+++ src/test/java/com/example/github_actions_test/GithubActionsTestApplicationTests.java
@@ -3,11 +3,21 @@ package com.example.github_actions_test;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class GithubActionsTestApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void successfulTest() {
+		System.out.println("Successful test");
+		assertEquals(true, true);
+	}
+
+	@Test
+	void failedTest() {
+		System.out.println("Failed test");
+		assertEquals(true, false);
 	}
 
 }
