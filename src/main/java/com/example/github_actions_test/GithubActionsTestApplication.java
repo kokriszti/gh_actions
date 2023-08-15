f0527b89ce046ac1bde169c46fa27bcb8742db13
Krisztina Pelei <krisztina.pelei@nttdata.com>
2023-08-15 16:53:38 +0200

    picture handler & controller

--- src/main/java/com/example/github_actions_test/GithubActionsTestApplication.java
+++ src/main/java/com/example/github_actions_test/GithubActionsTestApplication.java
@@ -1,13 +1,39 @@
 package com.example.github_actions_test;
 
+import com.example.github_actions_test.picturehandling.PictureHandler;
+import com.example.github_actions_test.picturehandling.PictureHandlerRefactor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class GithubActionsTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionsTestApplication.class, args);
+
+		//		PictureHandler pictureHandler = new PictureHandler();
+		//		pictureHandler.setPictureFilePath("C:\\Users\\krisztina.pelei\\Kriszti\\java_gyakorlas\\github_actions_test\\src\\main\\java\\com\\example\\github_actions_test\\caf.jpg");
+		//
+		//		if (pictureHandler.getPictureBytes() != null) {
+		//			System.out.println("success");
+		//		} else {
+		//			System.out.println("fail");
+		//		}
+
+		PictureHandlerRefactor pictureHandler2 = new PictureHandlerRefactor();
+		pictureHandler2.setPictureFilePath("src/main/java/com/example/github_actions_test/caf.jpg");
+		//		pictureHandler2.setPictureFilePath("C:\\Users\\krisztina.pelei\\Kriszti\\java_gyakorlas\\github_actions_test\\src\\main\\java\\com\\example\\github_actions_test\\caf.jpg");
+
+		try {
+			pictureHandler2.getPictureBytes();
+			System.out.println("success");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
--- src/main/java/com/example/github_actions_test/HelloController.java
+++ src/main/java/com/example/github_actions_test/HelloController.java
@@ -1,22 +1,56 @@
 package com.example.github_actions_test;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+@Controller
 public class HelloController {
 
-  @GetMapping("/")
-  public String hello() {
-    return "hello";
+//  if no endpoint on "/" path, index.html is served as default
+//  @GetMapping("/")
+//  @ResponseBody
+//  public String hello() {
+//    return "hello";
+//  }
+
+//  serve index.html on an endpoint
+  @GetMapping("/car")
+  public String car() {
+    return "index.html";
   }
+
+  @GetMapping("/public")
+  @ResponseBody
+  public String publicResource() {
+    return "This endpoint is available for everyone.";
+  }
+
+  @GetMapping("/protected")
+  @ResponseBody
+  public String protectedResource() {
+    return "This endpoint is protected.";
+  }
+
+//  if @RestController instead of @Controller
+
+//  @GetMapping("/")
+//  public String hello() {
+//    return "hello";
+//  }
+
+//  @GetMapping("/public")
+//  public String publicResource() {
+//    return "This endpoint is available for everyone";
+//  }
+//
+//  @GetMapping("/protected")
+//  public String protectedResource() {
+//    return "This endpoint is protected";
+//  }
 
 }
 
-//test commit
-//test commit
-//test commit
-//test commit
-//test commit
-//test commit
